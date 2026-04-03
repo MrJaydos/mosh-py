07f7c5ac0ba6cd802c1e94348450a5d809f10bc8
Jayden Dickinson <jaydend93@gmail.com>
2026-04-03 22:57:37 +1100

    Wrote the main game logic for the if statement. This will tell the user if they did good or not.

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -7,16 +7,34 @@ random_number = random.randint(1, 100)
 # Variable to keep the game running until the game no longer needs to be running
 running = True
 
+# Keeping track with the tries variable
+tries = 0
+
 
 # While loop so the user is prompted for input if they input the wrong infomation the first time. 
 while running == True:
     try:
+        
         # User input for the guess.
         guess = int(input("Guess a number between 1 & 100: "))
+        
+        # If statement to check if the number is below the the correct guess and then adds one number to tries variable
+        if guess < random_number:
+            tries += 1
+            print(f"Sorry! {guess} is lower than the correct guess! Try again!")
+        
+        # If statement to check if the number is above the the correct guess and then adds one number to tries variable
+        elif guess > random_number:
+            tries += 1
+            print(f"Sorry! {guess} is higher than the correct guess! Try again!")
+            
 
-
-
-
+        # If statement to check if the number is correct and then tells the lucky winner they won in x amount of tries! 
+        elif guess == random_number:
+            tries += 1
+            print(f"WOW! You got it! It only took {tries} times to guess the correct number!")
+        
+    # Error handling for silly duffas that put the wrong info into the console. 
     except ValueError:
         print("That is not a valid number. Please try again with a number like '12' and not a word.")
 
