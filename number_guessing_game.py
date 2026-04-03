385f991e6319c11b1242d5f2ef5438b2b32143b5
Jayden Dickinson <jaydend93@gmail.com>
2026-04-03 22:50:23 +1100

    Added conditions to the while loop and added the random number generator

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -1,14 +1,22 @@
 # Import random for generating random number between 1 & 100
 import random
 
+# Generates the random number to guess against
+random_number = random.randint(1, 100)
+
+# Variable to keep the game running until the game no longer needs to be running
+running = True
+
+
 # While loop so the user is prompted for input if they input the wrong infomation the first time. 
-while True:
+while running == True:
     try:
         # User input for the guess.
         guess = int(input("Guess a number between 1 & 100: "))
-        
-        # if the line above succeeds, the code will continue
-        break
+
+
+
+
     except ValueError:
         print("That is not a valid number. Please try again with a number like '12' and not a word.")
 
